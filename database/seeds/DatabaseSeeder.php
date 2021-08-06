450f3c04f85022852bd72e6c0d5c8cf5a03a609c
HaniyehAsiliyan <haniyeh.asn@gmail.com>
2021-08-06 20:13:31 +0430

    Refactoring Routes And Fix Issues

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        //$this->call(RoleAndPermissionSeeder::class);
+
+        $roleInDatabase = \Spatie\Permission\Models\Role::where('name',config('permission.default_roles')[0]);
+        if ($roleInDatabase->count() < 1){
+            foreach (config('permission.default_roles') as $role){
+                \Spatie\Permission\Models\Role::create([
+                    'name' => $role
+                ]);
+            }
+        }
+
+        $permissionInDatabase = \Spatie\Permission\Models\Permission::where('name',config('permission.default_permissions')[0]);
+        if ($permissionInDatabase->count() < 1){
+            foreach (config('permission.default_permissions') as $permission){
+                \Spatie\Permission\Models\Permission::create([
+                    'name' => $permission
+                ]);
+            }
+        }
     }
 }
