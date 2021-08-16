b99af80156c90312ad9c1e6ea5724b087ff78d5a
HaniyehAsiliyan <haniyeh.asn@gmail.com>
2021-08-16 22:25:20 +0430

    Added Updated Answers And Tests

--- tests/Feature/API/v1/Thread/AnswerTest.php
+++ tests/Feature/API/v1/Thread/AnswerTest.php
@@ -2,6 +2,7 @@
 
 namespace tests\Feature\API\v1\Thread;
 
+use App\Answer;
 use App\channel;
 use App\Thread;
 use App\User;
@@ -43,6 +44,41 @@ class AnswerTest extends TestCase
         ]);
 
         $response->assertStatus(Response::HTTP_CREATED);
+        $response->assertJson([
+            'message' => 'answer submitted successfully'
+        ]);
         $this->assertTrue($thread->answers()->where('content', 'Foo')->exists());
+    }
+
+    /** @test */
+    public function update_answer_should_be_validated()
+    {
+        $answer = factory(Answer::class)->create();
+
+        $response = $this->putJson(route('answers.update',[$answer]),[]);
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $response->assertJsonValidationErrors(['content']);
+    }
+
+    /** @test */
+    public function can_update_answer_of_thread()
+    {
+        Sanctum::actingAs(factory(User::class)->create());
+
+        $answer = factory(Answer::class)->create([
+            'content' => 'Foo',
+        ]);
+
+        $response = $this->putJson(route('answers.update',[$answer]),[
+            'content' => 'Bar',
+        ]);
+
+        $answer->refresh();
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJson([
+            'message' => 'answer updated successfully'
+        ]);
+        $this->assertEquals('Bar',$answer->content);
     }
 }
