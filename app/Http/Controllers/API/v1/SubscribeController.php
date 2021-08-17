7b31924946095e170c5c3d5172274a9273627c6f
HaniyehAsiliyan <haniyeh.asn@gmail.com>
2021-08-17 20:32:31 +0430

    Added Subscribe And Unsubscribe Controller

---
+++ app/Http/Controllers/API/v1/SubscribeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Http\Controllers\Controller;
+use App\Subscribe;
+use App\Thread;
+use Symfony\Component\HttpFoundation\Response;
+
+class SubscribeController extends Controller
+{
+    public function subscribe(Thread $thread)
+    {
+        auth()->user()->subscribes()->create([
+            'thread_id' => $thread->id
+        ]);
+
+        return response()->json([
+            'message' => 'User Subscribed Successfully'
+        ],Response::HTTP_OK);
+    }
+
+    public function unSubscribe(Thread $thread)
+    {
+        Subscribe::query()->where([
+            ['thread_id', $thread->id],
+            ['user_id', auth()->id()]
+        ])->delete();
+
+        return response()->json([
+            'message' => 'User Unsubscribed Successfully'
+        ],Response::HTTP_OK);
+    }
+}
