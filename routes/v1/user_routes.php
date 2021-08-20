b9515695a58f308babd0372f2da80131e1322b8f
HaniyehAsiliyan <haniyeh.asn@gmail.com>
2021-08-20 15:03:13 +0430

    Added User Routes

---
+++ routes/v1/user_routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\API\v1\User\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('/users')->group(function (){
+
+    Route::post('/leaderboards',[
+        UserController::class,
+        'leaderboards'
+    ])->name('users.leaderboards');
+
+});
